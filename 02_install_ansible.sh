a6e89992b587a81975cc14c1df26ef21133dfbaa
Jere Vekka <39953400+Jvekka@users.noreply.github.com>
2021-09-10 12:40:56 +0300

    Added support for Azure
    Azure requirements are missing most of the time so now you have it.

--- 02_install_ansible.sh
+++ 02_install_ansible.sh
@@ -1,8 +1,16 @@
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-#sudo apt-get install ansible
+
+# For some reason, the python3-pip install with .sh did fail for me.
+# Run manually if needed.
 sudo apt install python3-pip
 sudo pip3 install pywinrm
 sudo pip3 install pyvmomi
 sudo pip3 install ansible
 sudo pip3 install ansible[azure]
+
+# Added support for Azure
+# https://github.com/ansible-collections/azure
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo pip3 install -r requirements-azure.txt
+ansible-galaxy collection install azure.azcollection
